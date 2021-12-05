a2f3a2d38a54528cf467b8f9a17ad4d38edfd173
Jose Mendoza <developermendoza@gmail.com>
2021-12-04 16:30:56 -0800

    test

--- server/index.js
+++ server/index.js
@@ -31,14 +31,14 @@ app.use(express.json());
 console.log("process.env.NODE_ENV: ", process.env.NODE_ENV)
 
 // Serve static assets if in production
-// if(process.env.NODE_ENV === "production"){
-// //   //set static folder
-//   app.use(express.static('client/build'));
+if(process.env.NODE_ENV === "production"){
+//   //set static folder
+  app.use(express.static('client/build'));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//   })
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
