d75f2cc8cba897377c4d7a3e36c6546fc247c566
Jose Mendoza <developermendoza@gmail.com>
2021-12-04 16:33:33 -0800

    testing

--- server/index.js
+++ server/index.js
@@ -33,10 +33,10 @@ console.log("process.env.NODE_ENV: ", process.env.NODE_ENV)
 // Serve static assets if in production
 if(process.env.NODE_ENV === "production"){
 //   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'))
   })
 }
 
