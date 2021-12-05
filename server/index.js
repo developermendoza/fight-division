7f3a72dc51205872ce477d3a436a4e93bbe49647
Jose Mendoza <developermendoza@gmail.com>
2021-12-05 15:20:15 -0800

    testing

--- server/index.js
+++ server/index.js
@@ -36,15 +36,9 @@ app.use(express.json());
 // Serve static assets if in production
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
-  app.get('/*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"), function(err) {
-       if (err) {
-         console.log("__DIRNAME: ", __dirname)
-         console.log("testing testing testing")
-          res.status(500).send(err)
-       }
-    })
- })
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.use(function(req, res, next) {
